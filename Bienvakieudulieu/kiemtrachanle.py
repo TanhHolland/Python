0a20d75989936ba319303eff139fed1079faf630
Trần Anh <you@example.com>
2023-08-14 14:11:48 +0700

    lan dau

---
+++ Bienvakieudulieu/kiemtrachanle.py
@@ -0,0 +1,7 @@
+print("Input")
+x = input()
+print(" + ",end = '')
+y = input()
+print("= ",end = '')
+z = input()
+print("ho")
---
+++ Bienvakieudulieu/lamtronso.py
@@ -0,0 +1,10 @@
+x = input()
+x = int(x)
+while(x > 0) :
+    y = input()
+    y = int(y)
+    c = False
+    if(y % 10 == 5) c = True
+    while(y > 10) :
+        
+    
