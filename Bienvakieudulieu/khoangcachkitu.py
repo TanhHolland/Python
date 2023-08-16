b24d5aaa26ea17e8b0046984a793f4e497977701
Trần Anh <you@example.com>
2023-08-16 09:42:06 +0700

    chiu

--- Bienvakieudulieu/khoangcachkitu.py
+++ Bienvakieudulieu/khoangcachkitu.py
@@ -1,1 +1,15 @@
-print('a' - 'b')
+x = int(input())
+def check(s):
+    sd = s[::-1]
+    for i in range(1,len(s)):
+        a = ord(s[i])
+        b = ord(s[i-1])
+        c = ord(sd[i])
+        d = ord(sd[i-1])
+        if abs(a - b) != abs(c - d) : return "NO"
+    return "YES"
+while x > 0 :
+    x-=1
+    s = input()
+    print(check(s))
+    
