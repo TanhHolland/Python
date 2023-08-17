6e9dcb6a96fcdfbddf5d35ef92f84d270913960f
Trần Anh <you@example.com>
2023-08-17 10:18:05 +0700

    40

--- Bienvakieudulieu/soxenke.py
+++ Bienvakieudulieu/soxenke.py
@@ -1,14 +1,10 @@
 x = int(input())
 def check(n):
     if len(n) %2 == 0: return "NO"
-    if(n[0] == n[1]) return "NO"
+    if(n[0] == n[1]): return "NO"
     for i in range(2,len(n),2):
-        if (n[i] != )
-    sum = str(sum)
-    if(len(sum) < 2) :return "NO"
-    sum2 = sum[::-1]
-    if(sum2 == sum): return "YES"
-    return "NO"
+        if (n[i] != n[i-2]): return "NO"
+    return "YES"
 while x > 0:
     x-=1
     n = input()
