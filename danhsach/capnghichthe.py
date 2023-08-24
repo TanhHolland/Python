0618a5c15a4fb315273c1757328798906d62c9d4
Trần Anh <you@example.com>
2023-08-24 10:07:51 +0700

    1

--- danhsach/capnghichthe.py
+++ danhsach/capnghichthe.py
@@ -2,5 +2,6 @@ x = int(input())
 s = list(map(int,input().split()))
 c = 0
 for i in range(0,len(s)-1) :
-    if s[i] != s[i+1] : c+=1
+    for j in range(i+1,len(s)):
+        if s[i] > s[j] : c+=1
 print(c)
