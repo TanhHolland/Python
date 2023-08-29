cf3d67f6067054076f9c3ce9e9e8de156959a82e
Trần Anh <you@example.com>
2023-08-29 21:05:44 +0700

    1

--- danhsach/test.py
+++ danhsach/test.py
@@ -1,13 +1,8 @@
-x = int(input())
-while x > 0:
-    x-=1
-    n = int(input())
-    a = list(map(int,input().split()))
-    b = list(map(int,input().split()))
-    a.sort()
-    b.sort()
-    c = True
-    for i in range(0,n):
-        if a[i] > b[i] : c = False
-    if c == True : print("YES")
-    else : print ("NO")
+from math import gcd
+a = [1, 2, 3, 6]
+b = [0,0,0,0]
+k = 6
+d = -1
+for i in range(0,len(a)) :
+    if a[i] % k == 0 :
+        
