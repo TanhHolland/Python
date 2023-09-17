95c247b07f955eda4cfd35a1f88ebb2b567d35d2
tanhh <nhoklilom2.0@gmail.com>
2023-09-17 21:33:11 +0700

    1

--- danhsach/test.py
+++ danhsach/test.py
@@ -1,35 +1,27 @@
-n,m = map(int,input().split())
-t = n
-s = []
+t = int(input())
+
+
+def check(item):
+   return (item[0], item[1])
+
+
 while t > 0:
-   t-=1
-   xau = list(map(int,input().split()))
-   s.append(xau)
-if n > m :
-   dong = n-m
-   dem = 0
-   for i in range(0,len(s)) :
-      if i % 2 == 0 and dem < dong:
-         dem+=1
-         continue
-      for j in s[i] :
-         print(j,end= " ")
-      print()
-elif n < m :
-   cot = m - n
-   for i in range(0,len(s)) :
-      dem = 0
-      for j in range(0,len(s[i])) :
-         if j % 2 == 1 and dem < cot:
-            dem+=1
-            continue
-         print(s[i][j],end=" ")
-      print()
-else :
-   for i in range(len(s)) :
-      for j in range(len(s[i])) :
-         print(s[i][j],end=" ")
-      print()
-            
-   
-    
+   t -= 1
+   n = int(input())
+   ds = []
+   while n > 0:
+      n -= 1
+      a, b = list(map(int, input().split()))
+      tup = (a, b)
+      ds.append(tup)
+   ds.sort()
+   print(ds)
+   kq = []
+   kq.append(ds[0])
+   for i in range(1, len(ds)):
+      tup = kq[-1]
+      if ds[i][0] >= tup[1]:
+         kq.append(ds[i])
+      else:
+         kq.pop()
+   print(kq)
