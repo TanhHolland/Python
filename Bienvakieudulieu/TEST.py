0ee40b391be1e1f918a62b75f8606fe13ef4fd39
tanhh <nhoklilom2.0@gmail.com>
2023-09-25 12:24:42 +0700

    1

--- Bienvakieudulieu/TEST.py
+++ Bienvakieudulieu/TEST.py
@@ -1,25 +1,33 @@
 n = int(input())
-a = [0]*(n+1)
-a[0] = 1
-a[1] = 1
-for i in range(2, n//2+1):
-    if a[i] == 0:
-        for j in range(i*2, n+1, i):
-            if a[j] == 0:
-                a[j] = i
-for i in range(2, n+1):
-    if a[i] == 0:
-        a[i] = i
-ds = []
-result = 0
-for i in range(2, n+1):
-    x = i
-    count = 0
-    while i > 1:
-        count += 1
-        i = i//a[i]
-    count += 1
-    if count == 9:
-        result += 1
-        ds.append(x)
-print(len(ds))
+def check (s : str) :
+    a = list(map(int,list(s)))
+    # 4312 4132 4231 345
+    i = len(s)-2
+    while i >= 0 :
+        if a[i] < a[i+1] :
+            i-=1
+        else : 
+            break
+    if i < 0 : return '-1'
+    #  i = 1
+    MAX = -1
+    pos = len(s)-1
+    for j in range(len(s)-1,i,-1) :
+        if a[j] < MAX :
+            MAX = a[j]
+            pos = j
+    a[i],a[pos] = a[pos],a[i]
+    for x in range (i+1,len(s)-1) :
+        for y in range(x+1,len(s)) :
+            if a[x] < a[y] :
+                a[x],a[y] = a[y],a[x]
+    return ''.join(list(map(str,a)))
+while n > 0 :
+    n-=1
+    s = input()
+    print(check(s))
+    
+    
+    
+
+            
