a8ddbb3b1dc712f3a0f5384f8c1522cffa8cb905
tanhh <nhoklilom2.0@gmail.com>
2023-10-03 21:05:21 +0700

    mixi

--- Bienvakieudulieu/TEST.py
+++ Bienvakieudulieu/TEST.py
@@ -1,15 +1,18 @@
-ds = []
-def Try(s : str,n,a,b,c,d) :
-    if len(s) == n and a > 0 and b > 0 and c > 0 and d > 0 and int(s[-1]) % 2 == 1:
-        global ds
-        ds.append(int(s))
-    if len(s) < n :
-        Try(s + '2',n,a+1,b,c,d)
-        Try(s + '3',n,a,b+1,c,d)
-        Try(s + '5',n,a,b,c+1,d)
-        Try(s + '7',n,a,b,c,d+1)
-n = int(input())
-for i in range(4,n+1):
-    Try('',i,0,0,0,0)
-for i in ds :
-    print(i)
+a,b,m = list(map(int,input().split()))
+def ktrathuanngich(number,m) :
+    s = ""
+    while number > 0 :
+        sodu = number % m
+        s = str(sodu) + s
+        number //= m
+    return s == s[::-1]
+dem = 0
+for i in range(a,b+1) :
+    check  = True
+    for j in range(2,m+1):
+        if not ktrathuanngich(i,j) :
+            check = False
+            break
+    if check:
+        dem+=1
+print(dem)
