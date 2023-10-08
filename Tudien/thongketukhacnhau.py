4040cd3148f7e1a70a342a5751c9242497acf641
Your Name01~ <you@example.com>
2023-10-09 02:11:13 +0700

    mixi

---
+++ Tudien/thongketukhacnhau.py
@@ -0,0 +1,22 @@
+t = int(input())
+s = ""
+kt = " " 
+# Không phải các dấu như đề bài cho mà là tất cả trừ a-z A-Z 0-9
+while t > 0:
+    t -= 1
+    s += input() + " "
+s = s.lower()
+for i in range(48, 58): kt += chr(i)
+for i in range(65, 91): kt += chr(i)
+for i in range(97, 123): kt += chr(i) 
+for i in s:
+    if i not in kt:
+        s = s.replace(i," ")
+s = s.split()
+b = list(set(s))
+a = []
+for i in b :
+    a.append((i,s.count(i)))
+a = sorted(a,key=lambda x : (-x[1],x[0]))
+for i in a :
+    print(i[0],i[1])
